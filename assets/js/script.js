65e66e4033d98c8d5c89a16328c3f0933dfffd53
Lonny Vberg <lonnyvandenberg@gmail.com>
2021-09-04 22:42:41 -0600

    fixed alert error for past searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,41 +6,50 @@ var humidity = document.getElementById('humidity');
 var uvIndex = document.getElementById('uvIndex');
 var img = document.getElementById('img');
 var displayDate= document.getElementById('displayDate');
+var pastSearchesButton = document.getElementById('.pastSearchesButton');
 
+var cities = []
 
-button.addEventListener('click', function(){
-    event.preventDefault()
-var inputValue = document.getElementById('inputValue').value;
-//console.log(inputValue);
-fiveDay(inputValue)
-    fetch ('https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&units=imperial&APPID=f9d491be2c04cabd7a30f6d0ce1b0f1e')
-    .then(response => response.json())
-    .then(data => {
-        //console.log(data)
-        var nameValue = data['name'] + " ";
-        var date = moment();
-        displayDate.innerText= date;
-        img.setAttribute("src", `https://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`);;
-        var tempValue = data['main']['temp'] + " degrees";
-        //console.log(nameValue,tempValue)
-        city.innerText =  nameValue;
-        temp.innerText = "Temp: " + tempValue;
-        windSpeed.innerText = "Wind speed: " + data['wind']['speed']  + " MPH";
-        humidity.innerText = "Humidity: " + data['main']['humidity'] + "%";
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-        displayUvIndex(lat, lon)
+button.addEventListener('click', function(e){
+    e.preventDefault()
+    var inputValue = document.getElementById('inputValue').value;
+    fiveDay(inputValue)
+        fetch ('https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&units=imperial&APPID=f9d491be2c04cabd7a30f6d0ce1b0f1e')
+        .then(response => response.json())
+        .then(data => {
+            var nameValue = data['name'] + " ";
+            var date = moment();
+            displayDate.innerText= date;
+            img.setAttribute("src", `https://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`);;
+            var tempValue = data['main']['temp'] + " degrees";
+            city.innerText =  nameValue;
+            temp.innerText = "Temp: " + tempValue;
+            windSpeed.innerText = "Wind speed: " + data['wind']['speed']  + " MPH";
+            humidity.innerText = "Humidity: " + data['main']['humidity'] + "%";
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            displayUvIndex(lat, lon)
 
-        saveCity();
-    })
+            
+            saveCity();
+            searches();
+            console.log("test")
+        })
 
-    .catch(err => alert('Error' + err))
+        .catch(err => alert('Error' + err))
 })
-
+//Save current search to localStorage
 var saveCity = function () {
-    localStorage.setItem("inputValue", JSON.stringify(inputValue.value));
+    var newCity = inputValue.value
+    localStorage.setItem("City",JSON.stringify(cities));
     };
+//display past searches from localStorage
+var searches = function () {
+    var displayCity = JSON.parse(localStorage.getItem("City"));
+    console.log(displayCity);
 
+    //pastSearchesButton.innerHTML = displayCity
+}
 function displayUvIndex (lat, lon) {
     fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&APPID=f9d491be2c04cabd7a30f6d0ce1b0f1e")
     .then(response => response.json())
@@ -73,9 +82,6 @@ function fiveDay(city){
             <img src="https://openweathermap.org/img/wn/${forecast[i]['weather'][0]['icon']}@2x.png"/> </div>`
         }
         //console.log(HTMLText)
-        document.querySelector(".display2").innerHTML = HTMLText
+        //document.querySelector(".display2").innerHTML = HTMLText
     })
 }
-
-
-
